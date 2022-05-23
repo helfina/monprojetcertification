9928aab1561a1ec31009fc4f44cb3bc7da70b3c2
gaelle kerforne <stage.seeweb@gmail.com>
2022-05-23 16:33:05 +0200

    suppression de webpack, ajout header footer, avancement de fullcallendar et sa requete ajax
    Signed-off-by: Clément <stage.seeweb@gmail.com>

---
+++ src/Controller/AjaxController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Calendar;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/ajax')]
+class AjaxController extends AbstractController
+{
+    #[Route('/ajax', name: 'app_ajax')]
+    public function index(): Response
+    {
+        return $this->render('ajax/index.html.twig', [
+            'controller_name' => 'AjaxController',
+        ]);
+    }
+
+    #[Route('/ajax/{id}/edit', name: 'app_ajax_event_edit')]
+    public function updateEvent(?Calendar $calendar, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // On récupère les données
+        $donnees = json_decode($request->getContent());
+
+        if(
+            isset($donnees->title) && !empty($donnees->title) &&
+            isset($donnees->start) && !empty($donnees->start) &&
+            isset($donnees->description) && !empty($donnees->description) &&
+            isset($donnees->backgroundColor) && !empty($donnees->backgroundColor) &&
+            isset($donnees->borderColor) && !empty($donnees->borderColor) &&
+            isset($donnees->textColor) && !empty($donnees->textColor)
+        ){
+            // Les données sont complètes
+            // On initialise un code
+            $code = 200; //=> creations
+
+            // On vérifie si l'id existe
+            if(!$calendar){
+                // On instancie un rendez-vous
+                $calendar = new Calendar;
+
+                // On change le code
+                $code = 201; //=> j'ai crées
+            }
+
+            // On hydrate l'objet avec les données
+            $calendar->setTitle($donnees->title);
+            $calendar->setDescription($donnees->description);
+            $calendar->setStart(new DateTime($donnees->start));
+            if($donnees->allDay){
+                $calendar->setEnd(new DateTime($donnees->start));
+            }else{
+                $calendar->setEnd(new DateTime($donnees->end));
+            }
+            $calendar->setAllDay($donnees->allDay);
+            $calendar->setBacgroundColor($donnees->backgroundColor);
+            $calendar->setBorderColor($donnees->borderColor);
+            $calendar->setTextColor($donnees->textColor);
+
+            //$entityManager = $doctrine->getManager();
+            $entityManager->persist($calendar);
+            $entityManager->flush();
+            dump($entityManager);
+
+            // On retourne le code
+            return new Response('Ok', $code);
+        }else{
+            // Les données sont incomplètes
+            return new Response('Données incomplètes', 404);
+        }
+
+    }
+}
