a105cd6b96681887d3ec9dc8b7b5f5a038a5e48f
Gaelle <gkerforne@gmail.com>
2022-05-25 09:54:15 +0200

    home : suppression du calendrier pour l'afficher  dans l'agenda. ajout du fichier AgendaRepository.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,27 +10,8 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(CalendarRepository $calendarRepository): Response
-    {$events = $calendarRepository->findAll();
-
-        $rdvs = [];
-
-        foreach($events as $event){
-            $rdvs[] = [
-                'id' => $event->getId(),
-                'start' => $event->getStart()->format('Y-m-d H:i:s'),
-                'end' => $event->getEnd()->format('Y-m-d H:i:s'),
-                'title' => $event->getTitle(),
-                'description' => $event->getDescription(),
-                'backgroundColor' => $event->getBacgroundColor(),
-                'borderColor' => $event->getBorderColor(),
-                'textColor' => $event->getTextColor(),
-                'allDay' => $event->isAllDay(),
-            ];
-        }
-
-        $data = json_encode($rdvs);
-
-        return $this->render('home/index.html.twig', compact('data'));
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
     }
 }
