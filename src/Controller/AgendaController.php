8c317e9cd39aca626e0f5eabef7bda8530f5db22
Gaelle <gkerforne@gmail.com>
2022-05-31 15:22:24 +0200

    agenda: ajout de la requete api vacance scolaire

--- src/Controller/AgendaController.php
+++ src/Controller/AgendaController.php
@@ -6,21 +6,89 @@ use App\Entity\Agenda;
 use App\Form\AgendaType;
 use App\Repository\AgendaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use DateTime;
 
 class AgendaController extends AbstractController
 {
+
     #[Route('/agenda', name: 'app_agenda')]
-    public function index(AgendaRepository $agendaRepository): Response
+    public function index(?Agenda $agenda, AgendaRepository $agendaRepository, Request $request): Response
     {
+        $rdvs = [];
+        $ch = curl_init();
+        try {
+            curl_setopt($ch, CURLOPT_URL, "https://data.education.gouv.fr/api/v2/catalog/datasets/fr-en-calendrier-scolaire/exports/json?limit=-1&offset=0&lang=fr&timezone=UTC");
+            curl_setopt($ch, CURLOPT_HEADER, false);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($ch, CURLOPT_MAXREDIRS, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+
+
+            $response = curl_exec($ch);
+            $donnees = json_decode($response);
+
+            $tableauDeDonnées = [];
+            $annee = date('Y');
+//            date('Y-m-d H:i:s')
+            foreach ($donnees as $donnee) {
+                foreach ($donnee as $key => $value) {
+                    if ($donnee->start_date >= $annee && $donnee->population != "Enseignants" && $value == "Rennes") {
+
+                        $agenda = new Agenda();
+
+                        $agenda->setTitle($donnee->description);
+
+                        $agenda->setStart(new DateTime($donnee->start_date));
+                        $agenda->setEnd(new DateTime($donnee->end_date));
+
+                        $agenda->setAllDay(1);
+                        $agenda->setBackgroundColor("#008000");
+                        $agenda->setTextColor("#000000");
+                        $agenda->setDescription("population : " . $donnee->population . "</br> vacances scolaire : " . $donnee->location . " </br>  Zone :  " .
+                            $donnee->zones . "</br> années scolaire : " . $donnee->annee_scolaire
+
+                        );
+
+                        if ($this->isCsrfTokenValid('agenda' . $agenda->getId(), $request->request->get('_token'))) {
+                            dump($agenda);
+                            $agendaRepository->add($agenda, true);
+                        }
+                    }
+
+                }
+
+
+            }
+
+            if (curl_errno($ch)) {
+                echo curl_error($ch);
+                die();
+            }
+
+            $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            if ($http_code == intval(200)) {
+               // echo $response;
+
+            } else {
+                echo "Ressource introuvable : " . $http_code;
+            }
+        } catch (\Throwable $th) {
+            throw $th;
+        } finally {
+            curl_close($ch);
+        }
+
+
         $events = $agendaRepository->findAll();
 
-        $rdvs = [];
 
-        foreach($events as $event){
+        foreach ($events as $event) {
             $rdvs[] = [
                 'id' => $event->getId(),
                 'start' => $event->getStart()->format('Y-m-d H:i:s'),
@@ -42,7 +110,7 @@ class AgendaController extends AbstractController
     public function new(Request $request, AgendaRepository $agendaRepository): Response
     {
         $agenda = new Agenda();
-        $form= $this->createForm(AgendaType::class, $agenda);
+        $form = $this->createForm(AgendaType::class, $agenda);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -75,7 +143,7 @@ class AgendaController extends AbstractController
     #[Route('/agenda/edit/{id}', name: 'app_agenda_edit')]
     public function edit(Request $request, Agenda $agenda, AgendaRepository $agendaRepository): Response
     {
-        $form =$this->createForm(AgendaType::class, $agenda);
+        $form = $this->createForm(AgendaType::class, $agenda);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -92,7 +160,7 @@ class AgendaController extends AbstractController
     #[Route('/agenda/delete/{id}', name: 'app_agenda_delete')]
     public function delete(Request $request, Agenda $agenda, AgendaRepository $agendaRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$agenda->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $agenda->getId(), $request->request->get('_token'))) {
             $agendaRepository->remove($agenda, true);
         }
 
