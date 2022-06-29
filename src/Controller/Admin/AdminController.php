44b4b34b6fe3aaed68f01fcb9e40a402b8e753d0
Gaelle <gkerforne@gmail.com>
2022-06-29 11:46:37 +0200

    espace memebre :  ajout d'une route reserver a ladmin

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,11 +10,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    public function index(User $user, ): Response
     {
-        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
         // or add an optional message - seen by developers
         $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a page without having ROLE_ADMIN');
+
+
+
 
         return $this->render('administration/admin/index.html.twig', [
             'controller_name' => 'AdminController',
