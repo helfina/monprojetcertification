ef1cf5114f6a364f0a9dc17bcf9bfec7215dd64d
Gaelle <gkerforne@gmail.com>
2022-06-30 17:38:51 +0200

    header  ajout condition affichache btn administration style et script pour l'espace admin

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Application de gestions des calendrier de garde
+
+- calendrier ne peut etre modifier sans validation de l'autre parent
+- calendrier jugement par default
+- calendrier arrangement parental
+- afficher event semaine paire et impaire en fonction pere et mere avec une condition des vacances scolaire
+- aucun champs ne peut etre supprimer on sauvegarde une copie de la ligne modifier par qui et quand 
+- 
--- src/Controller/Admin/UsersController.php
+++ src/Controller/Admin/UsersController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +13,7 @@ class UsersController extends AbstractController
     #[Route('/users', name: 'app_users')]
     public function index(): Response
     {
+
         return $this->render('administration/users/index.html.twig');
     }
 }
